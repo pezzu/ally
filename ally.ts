adc015ec14e40d4f220e5406eb05646203586e31
pezzu <7243796+pezzu@users.noreply.github.com>
2020-04-16 23:45:54 -0400

    Cosmetic changes

--- ally.ts
+++ ally.ts
@@ -1,20 +1,15 @@
 import { spawn } from 'child_process';
 import { parse } from 'path';
-import * as fs from 'fs';
+import {readFileSync} from 'fs';
 
 const containsSequence = (master: any[], sub: any[]): boolean => sub.every((i => v => i = master.indexOf(v, i) + 1)(0));
 
 const replaceSequence = (array: any[], oldSeq: any[], newSeq: any[]): any[] => {
-  if (containsSequence(array, oldSeq) && (!containsSequence(array, newSeq) || (oldSeq.length > newSeq.length))) {
-    const position = array.indexOf(oldSeq[0]);
-    array = array.filter(item => !oldSeq.includes(item));
-    array.splice(position, 0, ...newSeq);
-  }
+  const position = array.indexOf(oldSeq[0]);
+  array = array.filter(item => !oldSeq.includes(item));
+  array.splice(position, 0, ...newSeq);
   return array;
 }
-
-const pipe = (...fns) => x => fns.reduce((y, f) => f(y), x );
-
  
 const getExeName = ():string => {
   return parse(__filename).name;
@@ -24,7 +19,7 @@ const readSettingsFromConfig = (command: string): Object => {
   const configFileName = `ally.config.json`;
   let config;
   try {
-    config = JSON.parse(fs.readFileSync(configFileName, 'utf8'));
+    config = JSON.parse(readFileSync(configFileName, 'utf8'));
   }
   catch (e) {
     console.error(`Problem reading or parsing ${configFileName}: `, e);
@@ -39,12 +34,20 @@ const readSettingsFromConfig = (command: string): Object => {
   return config[command];
 }
 
-const replaceParam = (params: string[], cmd: string, alias: string): string[] => {
-  return replaceSequence(params, cmd.split(' '), alias.split(' '));
+const requiresUpdate = (cmdLine: string[], oldParams: string[], newParams: string[]): boolean => {
+  return (containsSequence(cmdLine, oldParams) && (!containsSequence(cmdLine, newParams) || (oldParams.length > newParams.length)));
+}
+
+const replaceParam = (cmdLine: string[], oldParams: string[], newParams: string[]): string[] => {
+  if (requiresUpdate(cmdLine, oldParams, newParams)) {
+    cmdLine = replaceSequence(cmdLine, oldParams, newParams);  
+  }
+  
+  return cmdLine;
 }
   
 const replaceParams = (params: string[], aliases): string[] => {
-  return Object.entries(aliases).reduce((p, [cmd, alias]: [string, string]) => replaceParam(p, cmd, alias), params);
+  return Object.entries(aliases).reduce((p, [cmd, alias]: [string, string]) => replaceParam(p, cmd.split(' '), alias.split(' ')), params);
 }
 
 export const convertCommandLine = (argv: string[], options): string[] => {
